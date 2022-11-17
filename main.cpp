d94b9fc1fb6c31f6a6e8c92a5986b30fe76de7c2
salnyed <agorbuncova2004@gmail.com>
2022-11-17 16:43:05 +0400

    Add valueCalculation and operator []

--- main.cpp
+++ main.cpp
@@ -19,11 +19,12 @@ class Polynomial
 		}
 	};
 	Coeff* odds;
-	int highestDegree;
+	int numberOfCoeff;
  public:
 
-	void PushBack(const int data, const int degreeCoeff)
+	void AddCoeff(const int data, const int degreeCoeff)
 	{
+		if (data == 0) throw "Coefficient cannot be equal to zero";
 		Coeff* node = new Coeff(data, degreeCoeff);
 
 		if (odds == nullptr)
@@ -35,21 +36,27 @@ class Polynomial
 			Coeff* tmp = odds;
 			while (tmp->pNext)
 			{
+				if (degreeCoeff == tmp->degree)
+				{
+					tmp->number = data;
+					return;
+				}
 				tmp = tmp->pNext;
 			}
 			tmp->pNext = node;
 		}
-		highestDegree++;
+		numberOfCoeff++;
 	}
 
 	Polynomial(int degree = 1)
 	{
+		numberOfCoeff = 0;
 		if (degree < 1) throw "Error";
 		odds = nullptr;
 
 		for (int i = 0; i <= degree; ++i)
 		{
-			PushBack(1, i);
+			AddCoeff(1, i);
 		}
 	}
 
@@ -63,9 +70,42 @@ class Polynomial
 		Coeff* tmp = odds;
 		while (tmp)
 		{
-			std::cout << tmp->number << "^" << tmp->degree << "   ";
+			if (tmp->pNext) std::cout << tmp->number << "^" << tmp->degree << " + ";
+			else std::cout << tmp->number << "^" << tmp->degree << std::endl;
 			tmp = tmp->pNext;
 		}
+	}
+
+	int operator[](const int degree)
+	{
+		Coeff* tmp = this->odds;
+		while (tmp != nullptr)
+		{
+			if (degree == tmp->degree)
+			{
+				return tmp->number;
+			}
+			tmp = tmp->pNext;
+		}
+		return 0;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, Polynomial& obj)
+	{
+		obj.PrintPolynomial();
+		return os;
+	}
+
+	int valueCalculation(int x)
+	{
+		Coeff* tmp = odds;
+		int sum = 0;
+		for (int i = 0; i < numberOfCoeff; ++i)
+		{
+			sum += pow((odds->number * x), i + 1);
+		}
+
+		return sum;
 	}
 
 };
@@ -73,6 +113,8 @@ class Polynomial
 int main()
 {
 	Polynomial a = Polynomial(8);
-	a.PrintPolynomial();
+	Polynomial b = Polynomial(10);
+	std::cout << a;
+	std::cout << b.valueCalculation(1);
 	return 0;
 }
